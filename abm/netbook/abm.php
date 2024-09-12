82f04b21d2654b4c656e5ca0996fbf7bd5fddc50
Baldo <guillermo.baldo.586@alu.tecnica29de6.edu.ar>
2024-09-12 09:52:36 -0300

    notificacion de con fecha agregada

--- abm/netbook/abm.php
+++ abm/netbook/abm.php
@@ -71,6 +71,58 @@
     background-color: #c82333;
   }
 </style>
+<style>
+  /* Adjust modal width */
+  .modal-dialog, .modal-content {
+    width: 80% !important; /* Set a more reasonable width */
+    max-width: 80% !important;
+  }
+
+  /* Table adjustments */
+  table {
+    width: 100% !important;
+    table-layout: auto !important;
+    margin-bottom: 10px; /* Add slight margin to separate table from other elements */
+  }
+
+  /* Reduce padding inside the modal */
+  .modal-header, .modal-footer {
+    padding: 10px 15px !important; /* Less padding to make it more compact */
+  }
+
+  .modal-body {
+    padding: 15px !important;
+    overflow-x: hidden !important;
+  }
+
+  /* Input and select adjustments */
+  input, select, textarea {
+    max-width: 100% !important;
+    width: 100% !important;
+    margin-bottom: 5px; /* Reduce space between input elements */
+    font-size: 14px !important; /* Slightly smaller font size */
+  }
+
+  /* Adjust buttons for compact layout */
+  button {
+    padding: 5px 10px !important; /* Smaller padding for buttons */
+    font-size: 14px !important; /* Smaller font size */
+    margin: 5px; /* Reduce margin between buttons */
+  }
+
+  /* Adjust table row padding */
+  td, th {
+    padding: 8px !important; /* Reduce table cell padding */
+  }
+
+  /* Modal title adjustment */
+  .modal-title {
+    font-size: 16px !important; /* Slightly smaller modal title */
+  }
+</style>
+
+
+
 
 <?php
 include '../funciones.php';
@@ -97,7 +149,25 @@ try {
     $notifications = $sentencia->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  $consultaSQL = "SELECT registros.idregistro, users.user_name, recurso.recurso_nombre, DATE_FORMAT(registros.inicio_prestamo, '%d/%m %H:%i') AS inicio_prestamo, DATE_FORMAT(horario.horario, '%H:%i') AS horario, registros.devuelto FROM registros inner join recurso on recurso.recurso_id = registros.idrecurso inner join users on registros.idusuario = users.user_id inner join horario on horario.id = registros.fin_prestamo  where registros.devuelto = 'Pending'";
+  $consultaSQL = "SELECT 
+  registros.idregistro, 
+  users.user_name, 
+  recurso.recurso_nombre, 
+  DATE_FORMAT(registros.inicio_prestamo, '%d/%m %H:%i') AS inicio_prestamo, 
+  DATE_FORMAT(horario.horario, '%H:%i') AS horario, 
+  DATE_FORMAT(registros.fin_prestamo_fecha, '%d/%m/%Y') AS fecha_devolucion,  -- Agregada la fecha de devolución
+  registros.devuelto 
+FROM 
+  registros 
+INNER JOIN 
+  recurso ON recurso.recurso_id = registros.idrecurso 
+INNER JOIN 
+  users ON registros.idusuario = users.user_id 
+INNER JOIN 
+  horario ON horario.id = registros.fin_prestamo  
+WHERE 
+  registros.devuelto = 'Pending';
+";
   $sentencia = $conexion->prepare($consultaSQL);
   $sentencia->execute();
 
@@ -213,112 +283,153 @@ if ($error) {
         </button>
       </div>
       <div class="modal-body">
-        <?php
-        if (!empty($notifications)) {
-          echo '<table border="1">';
-          echo '<thead>';
-          echo '<tr>';
-          echo '<th><input type="checkbox" id="selectAllCheckbox"></th>'; // Checkbox para seleccionar/deseleccionar todos
-          echo '<th>Alumno</th>';
-          echo '<th>Material</th>';
-          echo '<th>Horario inicio</th>';
-          echo '<th>Horario de devolución</th>';
-          echo '<th>Fecha de devolución</th>'; // Nuevo campo de fecha
-          echo '<th></th>';
-          echo '</tr>';
-          echo '</thead>';
-          echo '<tbody>';
-        
-          foreach ($notifications as $notification) {
-            echo '<tr>';
-            // Checkbox para cada notificación
-            echo '<td><input type="checkbox" name="notifications[]" value="' . $notification['idregistro'] . '" class="checkboxNotification"></td>';
-            echo '<td><p id="notificationMessageUser">' . $notification['user_name'] . '</p></td>';
-            echo '<td><p id="notificationMessageResource">' . $notification['recurso_nombre'] . '</p></td>';
-            echo '<td><p id="notificationMessageStart">' . $notification['inicio_prestamo'] . '</p></td>';
-        
-            // Selector de horario
-            echo '<td>';
-            echo '<div class="form-group">';
-            echo '<select name="horario[' . $notification['idregistro'] . ']" id="horario" class="input">';
-            echo '<option value="" disabled hidden selected>Elegir un horario</option>';
-        
-            foreach ($datos as $dato) {
-              echo '<option value="' . $dato['id'] . '">' . $dato['horario'] . '</option>';
-            }
-        
-            echo '</select>';
-            echo '</div>';
-            echo '</td>';
-        
-            // Nuevo campo de fecha de devolución
-            echo '<td>';
-            echo '<input type="date" name="fin_prestamo_fecha[' . $notification['idregistro'] . ']" id="fechaDevolucion" class="input">';
-            echo '</td>';
-        
-            echo '<td>';
-            echo '<label for="nombreNetDevo"></label>';
-            echo '<select name="nombreNet" id="nombreNet" class="input">';
-            echo '<option value="' . $notification['recurso_nombre'] . '">' . $notification['recurso_nombre'] . '</option>';
-            echo '</select>';
-            echo '</td>';
-        
-            echo '</tr>';
-          }
-        
-          echo '</tbody>';
-          echo '</table>';
-        
-          echo '<tfoot>';
-          echo '<tr>';
-          echo '<td colspan="7" style="text-align: right;">';
-          echo '<div style="display: flex; justify-content: space-between; align-items: center; padding: 10px 0;">';
-        
-          // Menú desplegable para seleccionar el horario
-          echo '<select id="selectHorarioTodos" class="input" style="margin-right: auto; width: 60%;">';
-          echo '<option value="" disabled hidden selected>Elegir un horario para todos</option>';
-          foreach ($datos as $dato) {
-            echo '<option value="' . $dato['id'] . '">' . $dato['horario'] . '</option>';
-          }
-          echo '</select>';
-        
-          // Botón para aplicar el horario seleccionado a todos los selectores
-          echo '<button type="button" class="btn btn-outline-dark" onclick="selectAllTimesWithSelected()" style="padding-left: 36px; padding-right: 36px; margin-right: 15px;">Aplicar </button>';
-        
-          echo '</div>';
-          echo '</td>';
-          echo '</tr>';
-          echo '</tfoot>';
-        
-          // Footer con los botones
-          echo '<div class="modal-footer">';
-        
-          // Botones de aceptar y rechazar
-          echo '<div>';
-          echo '<button type="button" class="btn btn-success" id="acceptReturn" disabled style="padding-left: 22px; padding-right: 22px;">Aceptar</button>';
-        
-          echo '<button type="button" class="btn btn-danger" id="denyReturn" style="padding-left: 28px; padding-right: 28px;" disabled>Rechazar</button>';
-          echo '</div>';
-        
-          // Botones de marcar y desmarcar
-          echo '<div>';
-          echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxes(true)">Marcar todas</button>';
-          echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxes(false)">Desmarcar todas</button>';
-          echo '</div>';
-        
-          echo '</div>';
-        } else {
-          echo 'No hay notificaciones pendientes.';
-        }
-        
-        ?>
-        <script>
-  $(function() {
-    $("#fechaDevolucion").datepicker({
-      dateFormat: "yy-mm-dd" // Formato de fecha
+       <?php
+if (!empty($notifications)) {
+  echo '<table border="1">';
+  echo '<thead>';
+  echo '<tr>';
+  echo '<th><input type="checkbox" id="selectAllCheckbox"></th>'; // Checkbox para seleccionar/deseleccionar todos
+  echo '<th>Alumno</th>';
+  echo '<th>Material</th>';
+  echo '<th>Horario inicio</th>';
+  echo '<th>Horario de devolución</th>';
+  echo '<th>Fecha de devolución</th>'; // Nuevo campo de fecha
+  echo '<th></th>';
+  echo '</tr>';
+  echo '</thead>';
+  echo '<tbody>';
+
+  foreach ($notifications as $notification) {
+    echo '<tr>';
+    // Checkbox para cada notificación
+    echo '<td><input type="checkbox" name="notifications[]" value="' . $notification['idregistro'] . '" class="checkboxNotification"></td>';
+    echo '<td><p id="notificationMessageUser">' . $notification['user_name'] . '</p></td>';
+    echo '<td><p id="notificationMessageResource">' . $notification['recurso_nombre'] . '</p></td>';
+    echo '<td><p id="notificationMessageStart">' . $notification['inicio_prestamo'] . '</p></td>';
+
+    // Selector de horario
+    echo '<td>';
+    echo '<div class="form-group">';
+    echo '<select name="horario[' . $notification['idregistro'] . ']" id="horario" class="input">';
+    echo '<option value="" disabled hidden selected>Elegir un horario</option>';
+
+    foreach ($datos as $dato) {
+      echo '<option value="' . $dato['id'] . '">' . $dato['horario'] . '</option>';
+    }
+
+    echo '</select>';
+    echo '</div>';
+    echo '</td>';
+
+    // Nuevo campo de fecha de devolución con la fecha actual por defecto
+    echo '<td>';
+    echo '<input type="date" name="fin_prestamo_fecha[' . $notification['idregistro'] . ']" class="fechaDevolucion input">';
+    echo '</td>';
+
+    echo '<td>';
+    echo '<label for="nombreNetDevo"></label>';
+    echo '<select name="nombreNet" id="nombreNet" class="input">';
+    echo '<option value="' . $notification['recurso_nombre'] . '">' . $notification['recurso_nombre'] . '</option>';
+    echo '</select>';
+    echo '</td>';
+
+    echo '</tr>';
+  }
+
+  echo '</tbody>';
+  echo '</table>';
+
+  echo '<tfoot>';
+  echo '<tr>';
+  echo '<td colspan="7" style="text-align: right;">';
+  echo '<div style="display: flex; justify-content: space-between; align-items: center; padding: 10px 0;">';
+
+  // Menú desplegable para seleccionar el horario
+  echo '<select id="selectHorarioTodos" class="input" style="margin-right: auto; width: 60%;">';
+  echo '<option value="" disabled hidden selected>Elegir un horario para todos</option>';
+  foreach ($datos as $dato) {
+    echo '<option value="' . $dato['id'] . '">' . $dato['horario'] . '</option>';
+  }
+  echo '</select>';
+
+  // Botón para aplicar el horario seleccionado a todos los selectores
+  echo '<button type="button" class="btn btn-outline-dark" onclick="selectAllTimesWithSelected()" style="padding-left: 36px; padding-right: 36px; margin-right: 15px;">Aplicar horario a todos</button>';
+
+  echo '</div>';
+
+  // Campo de selección de fecha global y botón
+  echo '<div style="display: flex; justify-content: space-between; align-items: center; padding: 10px 0;">';
+
+  // Campo de fecha global
+  echo '<input type="date" id="selectFechaTodos" class="input" style="margin-right: auto; width: 60%;">';
+
+  // Botón para aplicar la fecha seleccionada a todos
+  echo '<button type="button" class="btn btn-outline-dark" onclick="selectAllDatesWithSelected()" style="padding-left: 36px; padding-right: 36px; margin-right: 15px;">Aplicar fecha a todos</button>';
+
+  echo '</div>';
+  echo '</td>';
+  echo '</tr>';
+  echo '</tfoot>';
+
+  // Footer con los botones
+  echo '<div class="modal-footer">';
+
+  // Botones de aceptar y rechazar
+  echo '<div>';
+  echo '<button type="button" class="btn btn-success" id="acceptReturn" disabled style="padding-left: 22px; padding-right: 22px;">Aceptar</button>';
+
+  echo '<button type="button" class="btn btn-danger" id="denyReturn" style="padding-left: 28px; padding-right: 28px;" disabled>Rechazar</button>';
+  echo '</div>';
+
+  // Botones de marcar y desmarcar
+  echo '<div>';
+  echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxes(true)">Marcar todas</button>';
+  echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxes(false)">Desmarcar todas</button>';
+  echo '</div>';
+
+  echo '</div>';
+} else {
+  echo 'No hay notificaciones pendientes.';
+}
+?>
+
+<script>
+  // Función para aplicar la misma fecha de devolución a todos los campos de fecha
+  function selectAllDatesWithSelected() {
+    const fechaSeleccionada = document.getElementById('selectFechaTodos').value;
+    if (fechaSeleccionada) {
+      document.querySelectorAll('.fechaDevolucion').forEach(function(input) {
+        input.value = fechaSeleccionada;
+      });
+    } else {
+      alert('Por favor selecciona una fecha.');
+    }
+  }
+
+  // Función para aplicar el mismo horario a todos los campos de horario
+  function selectAllTimesWithSelected() {
+    const horarioSeleccionado = document.getElementById('selectHorarioTodos').value;
+    if (horarioSeleccionado) {
+      document.querySelectorAll('#horario').forEach(function(select) {
+        select.value = horarioSeleccionado;
+      });
+    } else {
+      alert('Por favor selecciona un horario.');
+    }
+  }
+
+  // Establecer la fecha actual como valor por defecto en el campo de fecha global y en cada campo de fecha individual
+  document.addEventListener("DOMContentLoaded", function() {
+    const today = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato 'YYYY-MM-DD'
+    document.getElementById('selectFechaTodos').value = today;
+
+    // Aplicar la fecha actual a todos los campos de fecha individuales
+    document.querySelectorAll('.fechaDevolucion').forEach(function(input) {
+      input.value = today;
     });
   });
 </script>
+
 
 
       </div>
@@ -346,7 +457,7 @@ if ($error) {
           echo '<thead>';
           echo '<tr>';
           echo '<th><input type="checkbox" id="selectAllDevolucionCheckbox"></th>';  // Checkbox para seleccionar todos
-          echo '<th>Alumno</th>';
+          echo '<th>Usuario</th>';
           echo '<th>Material</th>';
           echo '<th>Horario inicio</th>';
           echo '<th>Horario final</th>';
@@ -355,51 +466,51 @@ if ($error) {
           echo '</tr>';
           echo '</thead>';
           echo '<tbody>';
-      
+
           foreach ($notificationDevolucion as $devolucion) {
-              echo '<tr>';
-              // Checkbox para seleccionar devoluciones
-              echo '<td><input type="checkbox" name="notificationDevolucion[]" value="' . $devolucion['idregistro'] . '" class="checkboxDevolucion"></td>';
-              echo '<td><p id="devolucionMessageUser">' . (isset($devolucion['user_name']) ? $devolucion['user_name'] : '') . '</p></td>';
-              echo '<td><p id="devolucionMessageResource">' . (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '') . '</p></td>';
-              echo '<td><p id="devolucionMessageStart">' . (isset($devolucion['inicio_prestamo']) ? $devolucion['inicio_prestamo'] : '') . '</p></td>';
-              echo '<td><p id="devolucionMessageEnd">' . (isset($devolucion['horario']) ? $devolucion['horario'] : '') . '</p></td>';
-      
-              // Nueva celda para la fecha de devolución
-              echo '<td><p id="devolucionFecha">' . (isset($devolucion['fecha_devolucion']) ? $devolucion['fecha_devolucion'] : 'No disponible') . '</p></td>';
-      
-              // Selector oculto con el nombre del recurso
-              echo '<td>';
-              echo '<label for="nombreNetDevo"></label>';
-              echo '<select name="nombreNetDevo[' . $devolucion['idregistro'] . ']" id="nombreNetDevo" class="input" style="display:none">';
-              echo '<option value="' . (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '') . '" class="input">';
-              echo (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '');
-              echo '</option>';
-              echo '</select>';
-              echo '</td>';
-              echo '</tr>';
+            echo '<tr>';
+            // Checkbox para seleccionar devoluciones
+            echo '<td><input type="checkbox" name="notificationDevolucion[]" value="' . $devolucion['idregistro'] . '" class="checkboxDevolucion"></td>';
+            echo '<td><p id="devolucionMessageUser">' . (isset($devolucion['user_name']) ? $devolucion['user_name'] : '') . '</p></td>';
+            echo '<td><p id="devolucionMessageResource">' . (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '') . '</p></td>';
+            echo '<td><p id="devolucionMessageStart">' . (isset($devolucion['inicio_prestamo']) ? $devolucion['inicio_prestamo'] : '') . '</p></td>';
+            echo '<td><p id="devolucionMessageEnd">' . (isset($devolucion['horario']) ? $devolucion['horario'] : '') . '</p></td>';
+
+            // Nueva celda para la fecha de devolución
+            echo '<td><p id="devolucionFecha">' . (isset($devolucion['fecha_devolucion']) ? $devolucion['fecha_devolucion'] : 'No disponible') . '</p></td>';
+
+            // Selector oculto con el nombre del recurso
+            echo '<td>';
+            echo '<label for="nombreNetDevo"></label>';
+            echo '<select name="nombreNetDevo[' . $devolucion['idregistro'] . ']" id="nombreNetDevo" class="input" style="display:none">';
+            echo '<option value="' . (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '') . '" class="input">';
+            echo (isset($devolucion['recurso_nombre']) ? $devolucion['recurso_nombre'] : '');
+            echo '</option>';
+            echo '</select>';
+            echo '</td>';
+            echo '</tr>';
           }
-      
+
           echo '</tbody>';
           echo '</table>';
-      
+
           // Botones de aceptar, rechazar, marcar y desmarcar todas
           echo '<div class="modal-footer">';
           echo '<div>';
           echo '<button type="button" class="btn btn-success" id="acceptDevolucion" style="padding-left: 22px; padding-right: 22px;" disabled>Aceptar</button>';
           echo '<button type="button" class="btn btn-danger" id="denyDevolucion" style="padding-left: 28px; padding-right: 28px;" disabled>Rechazar</button>';
           echo '</div>';
-      
+
           echo '<div>';
           echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxesDevolucion(true)">Marcar todas</button>';
           echo '<button type="button" class="btn btn-secondary btn-sm btn btn-outline-ligth" onclick="toggleCheckboxesDevolucion(false)">Desmarcar todas</button>';
           echo '</div>';
           echo '</div>';
-      } else {
+        } else {
           echo '<p>No hay devoluciones pendientes.</p>';
-      }
-      
-        
+        }
+
+
         ?>
 
 
